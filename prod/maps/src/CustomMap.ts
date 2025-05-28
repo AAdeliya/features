f414341b629245ef77ce9dce6205747041a5d9e1
firstPush <adel.sdet2@gmail.com>
2025-05-28 18:46:10 -0500

    showing popup windows 74

--- prod/maps/src/CustomMap.ts
+++ prod/maps/src/CustomMap.ts
@@ -1,3 +1,14 @@
+//instructions to every other class
+//on how they can be an argument to 'addMarker'
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+}
+
 export class CustomMap {
 private googleMap: google.maps.Map;
 
@@ -9,6 +20,29 @@ center: {
   lng:0
 }
 });
+
+  }
+
+  addMarker(mappable: Mappable): void {
+
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!'
+
+      });
+
+      infoWindow.open(this.googleMap, marker)
+    });
+
+  
 
   }
 
