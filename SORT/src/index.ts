23722a960c4f7f454980f9617da3a879d37374ff
firstPush <adel.sdet2@gmail.com>
2025-06-01 15:15:34 -0500

    sorting arbitrary collections

--- SORT/src/index.ts
+++ SORT/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollections } from "./NumbersCollections";
+import { CharactersCollections } from "./CharactersCollections";
 
-const numbersCollections = new NumbersCollections([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollections);
+// const numbersCollections = new NumbersCollections([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollections);
+// sorter.sort();
+// console.log(numbersCollections.data);
+
+const charactersCollections = new CharactersCollections('Xaayb');
+const sorter = new Sorter(charactersCollections);
 sorter.sort();
-console.log(numbersCollections.data);
+console.log(charactersCollections);
