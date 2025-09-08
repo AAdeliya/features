a653911dbdd2705575ef5faf442a03e1a8594c2f
firstPush <adel.sdet2@gmail.com>
2025-09-07 19:54:36 -0500

    NumbersCollections

--- SORT/src/index.ts
+++ SORT/src/index.ts
@@ -3,11 +3,11 @@ import { NumbersCollections } from "./NumbersCollections";
 import { CharactersCollections } from "./CharactersCollections";
 import { LinkedList } from "./LinkedList";
 
-/const numbersCollections = new NumbersCollections([10, 3, -5, 0]);
+const numbersCollections = new NumbersCollections([10, 3, -5, 0]);
 numbersCollections.sort();
 console.log(numbersCollections.data);
 
-// const charactersCollections = new CharactersCollections('Xaayb');
+const charactersCollections = new CharactersCollections('Xaayb');
 // charactersCollections.sort();
 // console.log(charactersCollections);
 
