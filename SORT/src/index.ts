08fa97e55cec458c5285e8fafb9677ef9277810c
firstPush <adel.sdet2@gmail.com>
2025-09-10 20:52:37 -0500

    numbersCollections

--- SORT/src/index.ts
+++ SORT/src/index.ts
@@ -9,7 +9,7 @@ console.log(numbersCollections.data);
 
 const charactersCollections = new CharactersCollections('Xaayb');
 charactersCollections.sort();
-// console.log(charactersCollections);
+console.log(charactersCollections);
 
 const linkedlist = new LinkedList();
 linkedlist.add(500);
